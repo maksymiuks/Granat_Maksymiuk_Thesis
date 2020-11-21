5ee0fc487150b5109e04d2e3673d6ff66860cfd0
granatb <32493624+granatb@users.noreply.github.com>
2020-11-21 19:14:48 +0100

    added html ui

--- questionnaire/app.r
+++ questionnaire/app.r
@@ -53,7 +53,7 @@ create_html_table <- function(data_row){
                           padding-left: .5em; 
                           padding-right: .5em;
                           font-family: sans-serif;
-                          box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+                          box-shadow: 0 0 5px rgba(0, 0, 0, 0.15);
                          ",
                          times = ncol(data_row)),
                      rep("border-collapse: collapse;
@@ -73,178 +73,182 @@ create_html_table <- function(data_row){
 }
 
 
-ui <- fluidPage(
-  shinyjs::useShinyjs(),
-  theme = shinytheme("yeti"),
-  h1("Annotations 0.0.1"),
-  tabsetPanel(
-    id = "tabset_panel",
-    tabPanel(
-      "Home",
-      div(style = "width: 600px; margin: 25px 0px 0px 0px;", 
-        
-        sidebarPanel(
-          width = 5,
-          h5(tags$b("Apllication's purpose is to acquire human explanations of phone prices by responding to 3 types of questions")),
-          h5("Application made as a part of a diploma by Bartłomiej Granat and Szymon Maksymiuk")#,
-        ),
-        
-        mainPanel(
-          width = 7,
-          h5(tags$b('Both sample size and questions number determine time required to fill the questionnaire.
-             For each additional 5 phones You will need 3-5 more minutes')),
-          splitLayout(
-            numericInput("sample_size", 
-                         "Sample size", 
-                         value = 10, 
-                         min = 5, 
-                         max = 200,
-                         width = 125),
-            numericInput("questions", 
-                         "Questions number", 
-                         value = 3, 
-                         min = 1, 
-                         max = 3,
-                         width = 125)
-          ),
-          div(style = "margin: 15px 0px 0px 0px;",
-              actionButton("data_browse", 
-                           "Browse sample", 
-                           width = 300)),
-          div(style = "margin: 15px 0px 0px 0px;",
-              actionButton("next_page", 
-                           "Answer questions",
-                           width = 300))
-        )
-      )
-    ),
-    tabPanel(
-      "Data",
-      DT::dataTableOutput("Data"),
-      div(style = "margin: 15px 0px 0px 0px;",
-          actionButton("next_page_data", 
-                       "Answer questions",
-                       width = 150))
-      ),
-    tabPanel(
-      "Questions",
-      uiOutput("MainAction"),
-      actionButton("Click.Counter", "Next")  
-    )
-    # tabPanel(
-    #   "2nd Question",
-    #   uiOutput("nd_question")
-    # ),
-    # tabPanel(
-    #   "3rd Question",
-    #   uiOutput("rd_question")
-    # )
-  )
+# ui <- fluidPage(
+#   shinyjs::useShinyjs(),
+#   theme = "bootstrap.css",
+#   h1("Annotations 0.0.1"),
+#   tabsetPanel(
+#     id = "tabset_panel",
+#     tabPanel(
+#       "Home",
+#       div(style = "width: 600px; margin: 25px 0px 0px 0px;", 
+#         
+#         sidebarPanel(
+#           width = 5,
+#           h5(tags$b("Apllication's purpose is to acquire human explanations of phone prices by responding to 3 types of questions")),
+#           h5("Application made as a part of a diploma by Bartłomiej Granat and Szymon Maksymiuk")#,
+#         ),
+#         
+#         mainPanel(
+#           width = 7,
+#           h5(tags$b('Both sample size and questions number determine time required to fill the questionnaire.
+#              For each additional 5 phones You will need 3-5 more minutes')),
+#           splitLayout(
+#             numericInput("sample_size", 
+#                          "Sample size", 
+#                          value = 10, 
+#                          min = 5, 
+#                          max = 200,
+#                          width = 125),
+#             numericInput("questions", 
+#                          "Questions number", 
+#                          value = 3, 
+#                          min = 1, 
+#                          max = 3,
+#                          width = 125)
+#           ),
+#           div(style = "margin: 15px 0px 0px 0px;",
+#               actionButton("data_browse", 
+#                            "Browse sample", 
+#                            width = 300)),
+#           div(style = "margin: 15px 0px 0px 0px;",
+#               actionButton("next_page", 
+#                            "Answer questions",
+#                            width = 300))
+#         )
+#       )
+#     ),
+#     tabPanel(
+#       "Data",
+#       DT::dataTableOutput("Data"),
+#       div(style = "margin: 15px 0px 0px 0px;",
+#           actionButton("next_page_data", 
+#                        "Answer questions",
+#                        width = 150))
+#       ),
+#     tabPanel(
+#       "Questions",
+#       uiOutput("MainAction"),
+#       actionButton("Click.Counter", "Next")  
+#     )
+#     # tabPanel(
+#     #   "2nd Question",
+#     #   uiOutput("nd_question")
+#     # ),
+#     # tabPanel(
+#     #   "3rd Question",
+#     #   uiOutput("rd_question")
+#     # )
+#   )
+# )
+
+
+ui <- shiny::htmlTemplate(
+  # Index Page
+  "www/index.html",
+  
+  # Number of trips
+  number_of_trips = textOutput(
+    "num_trips",
+    inline = T
+  ),
+  
+  sample_size = numericInput("sample_size", 
+                            "Sample size",
+                            value = 10,
+                            min = 5,
+                            max = 200,
+                            width = 125),
+  
+  questions_number = numericInput("questions",
+                                  "Questions number",
+                                  value = 3,
+                                  min = 1,
+                                  max = 3,
+                                  width = 125),
+  
+  data_browser = DT::dataTableOutput("Data"),
+  
+  questions = uiOutput("MainAction"),
+  
+  ccounter = actionButton("Click.Counter", "Next")
+  
 )
 
-server <- function(input, output, session) {
+
+server <- function(input, output) {
   
+  # Basic Numbers Page --------------------------------------------------------------
   
   data <- read.csv("phones.csv")
-  
-  
+
+
   data_sample <- reactive({
     m <- sample(1:nrow(data), input$sample_size)
     ret <- data[m,]
     rownames(ret) <- NULL
     ret
   })
-
-  
-  
-  observeEvent(input$data_browse, {
-    updateTabsetPanel(session, "tabset_panel", selected = "Data")
-  })
-  
-  observeEvent(input$next_page, {
-    updateTabsetPanel(session, "tabset_panel",selected = "Questions")
-  })
-  
-  observeEvent(input$next_page_data, {
-    updateTabsetPanel(session, "tabset_panel",selected = "Questions")
-  })
-  
-  observeEvent(input$close, {
-    stopApp()
-  })
-
   
   output$Data <- DT::renderDataTable({
     DT::datatable(data_sample())
   })
-  # 
-  # observeEvent(input$Click.Counter, { 
-  #   # print(input$Click.Counter[1])
-  #   #shinyjs::disable("Click.Counter")
-  #   if(input$Click.Counter > 1 & input$Click.Counter <= 4){
-  #     
-  #     print(input$Click.Counter[1])
-  #     
-  #   } else if(input$Click.Counter > 5){
-  #     print("Finished with covariates")
-  #   } else {
-  #     print("Initializing...")
-  #   }
-  # })
-  # 
+  
   output$MainAction <- renderUI( {
     dynamicUi()
   })
-  
-  
-  
+
+  observeEvent(input$close, {
+    stopApp()
+  })
+
   dynamicUi <- reactive({
     if (input$Click.Counter==0)
       return(
         list(
-          h5("Thank you for participating in our project. Please fill all of the following questions and submit your answers")
+          h5("Please fill all of the following questions and remember to SUBMIT your answers")
         )
       )
-    
+
     # Once the next button has been clicked once we see each question
     # of the survey.
     if (input$Click.Counter==1){
       # shinyjs::disable("Click.Counter")
       return(
         list(
-          h5("In this part please mark 3 most important features that determine phone price based on the whole sample"),
-          renderUI({
-            formUI(form_data_st())
-          })
-        )
-      )
-      
-    }
-    
-    if (input$Click.Counter==2){
-      #shinyjs::disable("Click.Counter")
-      return(
-        list(
-          h5("In this part please mark the impact of each single feature on the price"),
+          h5("Type 1/3: Looking at the data above please name 3 features affecting price of a telepohone the most"),
           renderUI({
             formUI(form_data_nd())
           })
         )
       )
+
     }
-    
+
+    if (input$Click.Counter==2){
+      #shinyjs::disable("Click.Counter")
+      return(
+        list(
+          h5("Type 2/3: For selected number of telephones please drag and drop the features so they are ordered from most to least important when you look at the telephone price"),
+          renderUI({
+            formUI(form_data_st())
+          })
+        )
+      )
+    }
+
     if (input$Click.Counter==3){
       #shinyjs::disable("Click.Counter")
       return(
         list(
-          h5("In this part please mark the impact of each single feature on the price"),
+          h5("Type 3/3: For one random telephone mark the type of impact on price of each single feature"),
           renderUI({
             formUI(form_data_rd())
           })
         )
       )
     }
-    
+
     if (input$Click.Counter==4){
       shinyjs::disable("Click.Counter")
       return(
@@ -254,49 +258,49 @@ server <- function(input, output, session) {
         )
       )
     }
-    
+
   })
-  
+
   form_data_st <- reactive({
     data <- data_sample() %>% mutate_if(is.factor, as.character)
     n <- nrow(data)
     id <- "st_Question"
-    
+
     questions <- lapply(1:n, function(x){
-      list(id = paste("st", as.character(x), sep = "_"), 
-           type = "order" , 
-           title = paste(x, "Order features according to its importance for a given observation", sep = " "), 
+      list(id = paste("st", as.character(x), sep = "_"),
+           type = "order" ,
+           title = paste0("Observation: ", as.character(x)),
            hint = HTML(create_html_table(data[x,])),
            choices = colnames(data))
     })
     storage <- list(type = STORAGE_TYPES$FLATFILE, path = "responses")
     list(id = id, questions = questions, storage = storage)
   })
- 
+
   form_data_nd <- reactive({
     data <- data_sample() %>% mutate_if(is.factor, as.character)
     n <- nrow(data)
     id <- "nd_Question"
-    questions <- lapply(1:n, function(x){
-      list(id = paste("nd", as.character(x), sep = "nd"), 
-           type = "checkbox_multi" , 
-           title = paste(x, "Name 3 most important features", sep = " "), 
-           hint = HTML(create_html_table(data[x,])),
+    questions <- lapply(1:1, function(x){
+      list(id = paste("nd", as.character(x), sep = "nd"),
+           type = "checkbox_multi" ,
+           title = "Mark the features",
+           hint = "",
            choices = as.list(colnames(data)),
            inline = TRUE)
     })
     storage <- list(type = STORAGE_TYPES$FLATFILE, path = "responses")
     list(id = id, questions = questions, storage = storage)
   })
-  
+
   form_data_rd <- reactive({
     data <- data_sample() %>% mutate_if(is.factor, as.character)
     n <- nrow(data)
     id <- "rd_Question"
     questions <- lapply(1:n, function(x){
-      list(id = paste("rd", as.character(x), sep = "rd"), 
-           type = "radio" , 
-           title = paste(x, "Variable", colnames(data)[sample(1:ncol(data), 1)], "contributes to price: ", sep = " "), 
+      list(id = paste("rd", as.character(x), sep = "rd"),
+           type = "radio" ,
+           title = paste(x, "Variable", colnames(data)[sample(1:ncol(data), 1)], "contributes to price: ", sep = " "),
            hint = HTML(create_html_table(data[x,])),
            choices = list("Highly positive", "Slightly positive", "Neutral", "Slightly negative", "Highly negative"),
            inline = TRUE)
@@ -304,28 +308,213 @@ server <- function(input, output, session) {
     storage <- list(type = STORAGE_TYPES$FLATFILE, path = "responses")
     list(id = id, questions = questions, storage = storage)
   })
-  
-  
-  # 
+
+
+  #
   # output$st_question <- renderUI({
   #   formUI(form_data_st())
   # })
-  # 
+  #
   # output$nd_question <- renderUI({
   #   formUI(form_data_nd())
   # })
-  
+
   output$rd_question <- renderUI({
     formUI(form_data_rd())
   })
-  
-  
+
+
   callModule(shinyforms:::formServerHelper, "st_Question", form_data_st)
   callModule(shinyforms:::formServerHelper, "nd_Question", form_data_nd)
   callModule(shinyforms:::formServerHelper, "rd_Question", form_data_rd)
   
-  
 }
+
+
+# server <- function(input, output, session) {
+#   
+#   
+#   data <- read.csv("phones.csv")
+#   
+#   
+#   data_sample <- reactive({
+#     m <- sample(1:nrow(data), input$sample_size)
+#     ret <- data[m,]
+#     rownames(ret) <- NULL
+#     ret
+#   })
+# 
+#   
+#   
+#   observeEvent(input$data_browse, {
+#     updateTabsetPanel(session, "tabset_panel", selected = "Data")
+#   })
+#   
+#   observeEvent(input$next_page, {
+#     updateTabsetPanel(session, "tabset_panel",selected = "Questions")
+#   })
+#   
+#   observeEvent(input$next_page_data, {
+#     updateTabsetPanel(session, "tabset_panel",selected = "Questions")
+#   })
+#   
+#   observeEvent(input$close, {
+#     stopApp()
+#   })
+# 
+#   
+#   output$Data <- DT::renderDataTable({
+#     DT::datatable(data_sample())
+#   })
+#   # 
+#   # observeEvent(input$Click.Counter, { 
+#   #   # print(input$Click.Counter[1])
+#   #   #shinyjs::disable("Click.Counter")
+#   #   if(input$Click.Counter > 1 & input$Click.Counter <= 4){
+#   #     
+#   #     print(input$Click.Counter[1])
+#   #     
+#   #   } else if(input$Click.Counter > 5){
+#   #     print("Finished with covariates")
+#   #   } else {
+#   #     print("Initializing...")
+#   #   }
+#   # })
+#   # 
+#   output$MainAction <- renderUI( {
+#     dynamicUi()
+#   })
+#   
+#   
+#   
+#   dynamicUi <- reactive({
+#     if (input$Click.Counter==0)
+#       return(
+#         list(
+#           h5("Thank you for participating in our project. Please fill all of the following questions and submit your answers")
+#         )
+#       )
+#     
+#     # Once the next button has been clicked once we see each question
+#     # of the survey.
+#     if (input$Click.Counter==1){
+#       # shinyjs::disable("Click.Counter")
+#       return(
+#         list(
+#           h5("In this part please mark 3 most important features that determine phone price based on the whole sample"),
+#           renderUI({
+#             formUI(form_data_st())
+#           })
+#         )
+#       )
+#       
+#     }
+#     
+#     if (input$Click.Counter==2){
+#       #shinyjs::disable("Click.Counter")
+#       return(
+#         list(
+#           h5("In this part please mark the impact of each single feature on the price"),
+#           renderUI({
+#             formUI(form_data_nd())
+#           })
+#         )
+#       )
+#     }
+#     
+#     if (input$Click.Counter==3){
+#       #shinyjs::disable("Click.Counter")
+#       return(
+#         list(
+#           h5("In this part please mark the impact of each single feature on the price"),
+#           renderUI({
+#             formUI(form_data_rd())
+#           })
+#         )
+#       )
+#     }
+#     
+#     if (input$Click.Counter==4){
+#       shinyjs::disable("Click.Counter")
+#       return(
+#         list(
+#           h5("That is everything we prepared for You. Thank You for participation"),
+#           actionButton("close", "Close")
+#         )
+#       )
+#     }
+#     
+#   })
+#   
+#   form_data_st <- reactive({
+#     data <- data_sample() %>% mutate_if(is.factor, as.character)
+#     n <- nrow(data)
+#     id <- "st_Question"
+#     
+#     questions <- lapply(1:n, function(x){
+#       list(id = paste("st", as.character(x), sep = "_"), 
+#            type = "order" , 
+#            title = paste(x, "Order features according to its importance for a given observation", sep = " "), 
+#            hint = HTML(create_html_table(data[x,])),
+#            choices = colnames(data))
+#     })
+#     storage <- list(type = STORAGE_TYPES$FLATFILE, path = "responses")
+#     list(id = id, questions = questions, storage = storage)
+#   })
+#  
+#   form_data_nd <- reactive({
+#     data <- data_sample() %>% mutate_if(is.factor, as.character)
+#     n <- nrow(data)
+#     id <- "nd_Question"
+#     questions <- lapply(1:n, function(x){
+#       list(id = paste("nd", as.character(x), sep = "nd"), 
+#            type = "checkbox_multi" , 
+#            title = paste(x, "Name 3 most important features", sep = " "), 
+#            hint = HTML(create_html_table(data[x,])),
+#            choices = as.list(colnames(data)),
+#            inline = TRUE)
+#     })
+#     storage <- list(type = STORAGE_TYPES$FLATFILE, path = "responses")
+#     list(id = id, questions = questions, storage = storage)
+#   })
+#   
+#   form_data_rd <- reactive({
+#     data <- data_sample() %>% mutate_if(is.factor, as.character)
+#     n <- nrow(data)
+#     id <- "rd_Question"
+#     questions <- lapply(1:n, function(x){
+#       list(id = paste("rd", as.character(x), sep = "rd"), 
+#            type = "radio" , 
+#            title = paste(x, "Variable", colnames(data)[sample(1:ncol(data), 1)], "contributes to price: ", sep = " "), 
+#            hint = HTML(create_html_table(data[x,])),
+#            choices = list("Highly positive", "Slightly positive", "Neutral", "Slightly negative", "Highly negative"),
+#            inline = TRUE)
+#     })
+#     storage <- list(type = STORAGE_TYPES$FLATFILE, path = "responses")
+#     list(id = id, questions = questions, storage = storage)
+#   })
+#   
+#   
+#   # 
+#   # output$st_question <- renderUI({
+#   #   formUI(form_data_st())
+#   # })
+#   # 
+#   # output$nd_question <- renderUI({
+#   #   formUI(form_data_nd())
+#   # })
+#   
+#   output$rd_question <- renderUI({
+#     formUI(form_data_rd())
+#   })
+#   
+#   
+#   callModule(shinyforms:::formServerHelper, "st_Question", form_data_st)
+#   callModule(shinyforms:::formServerHelper, "nd_Question", form_data_nd)
+#   callModule(shinyforms:::formServerHelper, "rd_Question", form_data_rd)
+#   
+#   
+# }
 
 
 
---
+++ questionnaire/form.html
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Gym Membership Form</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <style>
+      html, body {
+      min-height: 100%;
+      }
+      body, div, form, input, select, p { 
+      padding: 0;
+      margin: 0;
+      outline: none;
+      font-family: Roboto, Arial, sans-serif;
+      font-size: 14px;
+      color: #666;
+      line-height: 22px;
+      }
+      h1 {
+      position: absolute;
+      margin: 0;
+      font-size: 36px;
+      color: #fff;
+      z-index: 2;
+      }
+      span.required {
+      color: red;
+      }
+      .testbox {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: inherit;
+      padding: 20px;
+      }
+      form {
+      width: 100%;
+      padding: 20px;
+      border-radius: 6px;
+      background: #fff;
+      box-shadow: 0 0 30px 0 #095484; 
+      }
+      .banner {
+      position: relative;
+      height: 180px;
+      background-image: url("/uploads/media/default/0001/01/9e07ce3a601795a5ac09a66a0c1fc8978e0ee51a.jpeg");  
+      background-size: cover;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      text-align: center;
+      }
+      .banner::after {
+      content: "";
+      background-color: rgba(0, 0, 0, 0.4); 
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      }
+      p.top-info {
+      margin: 10px 0;
+      }
+      input, select {
+      margin-bottom: 10px;
+      border: 1px solid #ccc;
+      border-radius: 3px;
+      }
+      input {
+      width: calc(100% - 10px);
+      padding: 5px;
+      }
+      select {
+      width: 100%;
+      padding: 7px 0;
+      background: transparent;
+      }
+      .item:hover p, .question:hover p, .question label:hover, input:hover::placeholder {
+      color: #095484;
+      }
+      .item input:hover, .item select:hover {
+      border: 1px solid transparent;
+      box-shadow: 0 0 5px 0 #095484;
+      color: #095484;
+      }
+      .item {
+      position: relative;
+      margin: 10px 0;
+      }
+      .question input {
+      width: auto;
+      margin: 0;
+      border-radius: 50%;
+      }
+      .question input, .question span {
+      vertical-align: middle;
+      }
+      .question label {
+      display: inline-block;
+      margin: 5px 20px 15px 0;
+      }
+      .btn-block {
+      margin-top: 10px;
+      text-align: center;
+      }
+      button {
+      width: 150px;
+      padding: 10px;
+      border: none;
+      border-radius: 5px; 
+      background: #095484;
+      font-size: 16px;
+      color: #fff;
+      cursor: pointer;
+      }
+      button:hover {
+      background: #0666a3;
+      }
+      @media (min-width: 568px) {
+      .name-item, .city-item {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-between;
+      }
+      .name-item input, .city-item input {
+      width: calc(50% - 20px);
+      }
+      .city-item select {
+      width: calc(50% - 8px);
+      }
+      }
+    </style>
+  </head>
+  <body>
+    <div class="testbox">
+      <form action="/">
+        <div class="banner">
+          <h1>Gym Membership Form</h1>
+        </div>
+        <p class="top-info">Want to become a member of our Gym? Then start by filling our form to complete registration. We will contact you shortly to notify you about your membership card.</p>
+        <div class="item">
+          <p>Name<span class="required">*</span></p>
+          <div class="name-item">
+            <input type="text" name="name" placeholder="First" required/>
+            <input type="text" name="name" placeholder="Last" required/>
+          </div>
+        </div>
+        <div class="question">
+          <p>Gender<span class="required">*</span></p>
+          <div class="question-answer">
+            <label><input type="radio" value="none" name="gender" required/> <span>Male</span></label>
+            <label><input type="radio" value="none" name="gender" required/> <span>Female</span></label>
+          </div>
+        </div>
+        <div class="item">
+          <p>Your current weight (lbs)<span class="required">*</span></p>
+          <input type="text" name="name"/>
+        </div>
+        <div class="item">
+          <p>Desired weight (lbs)<span class="required">*</span></p>
+          <input type="text" name="name" required/>
+        </div>
+        <div class="item">
+          <p>Height<span class="required">*</span></p>
+          <input type="text" name="name" required/>
+        </div>
+        <div class="item">
+          <p>BMI<span class="required">*</span></p>
+          <input type="text" name="name" required/>
+        </div>
+        <div class="item">
+          <p>Home Address<span class="required">*</span></p>
+          <input type="text" name="name" placeholder="Street address" required/>
+          <div class="city-item">
+            <input type="text" name="name" placeholder="City" required/>
+            <input type="text" name="name" placeholder="Region" required/>
+            <input type="text" name="name" placeholder="Postal / Zip code" required/>
+            <select>
+              <option value="">Country</option>
+              <option value="1">Russia</option>
+              <option value="2">Germany</option>
+              <option value="3">France</option>
+              <option value="4">Armenia</option>
+              <option value="5">USA</option>
+            </select>
+          </div>
+        </div>
+        <div class="item">
+          <p>Email<span class="required">*</span></p>
+          <input type="text" name="name" required/>
+        </div>
+        <div class="item">
+          <p>Phone</p>
+          <input type="text" name="name"/>
+        </div>
+        <div class="question">
+          <p>Do you require a personal trainer?<span class="required">*</span></p>
+          <div class="question-answer">
+            <label><input type="radio" value="none" name="personal-trainer" required/> <span>Yes</span></label>
+            <label><input type="radio" value="none" name="personal-trainer" required/> <span>No</span></label>
+          </div>
+        </div>
+        <div class="question">
+          <p>Have you been in a Gym before?<span class="required">*</span></p>
+          <div class="question-answer">
+            <label><input type="radio" value="none" name="gym-before" required/> <span>Yes</span></label>
+            <label><input type="radio" value="none" name="gym-before" required/> <span>No</span></label>
+          </div>
+        </div>
+        <div class="question">
+          <p>Membership Type<span class="required">*</span></p>
+          <div class="question-answer">
+            <label><input type="radio" value="none" name="membership" required/> <span>Regular</span></label>
+            <label><input type="radio" value="none" name="membership" required/> <span>Pro</span></label>
+            <label><input type="radio" value="none" name="membership" required/> <span>VIP</span></label>
+          </div>
+        </div>
+        <div class="btn-block">
+          <button type="submit" href="/">Apply</button>
+        </div>
+      </form>
+    </div>
+  </body>
+</html>
