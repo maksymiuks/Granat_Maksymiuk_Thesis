e7fa0632013cfc30de37cbc93dcb22e733e7b5f2
maksymiuks <32574056+maksymiuks@users.noreply.github.com>
2021-01-18 21:19:55 +0100

    Add lime

--- scripts/model.R
+++ scripts/model.R
@@ -4,10 +4,10 @@ phones <- phones[,-c(9, 10)]
 
 library(mlr)
 
-task <- makeRegrTask("phones", data[,-c(1)], "price")
+task <- makeRegrTask("phones", phones[,-c(1)], "price")
 lrn <- makeLearner("regr.gbm", par.vals = list(n.trees = 2672, interaction.depth = 2, n.minobsinnode=5, shrinkage=0.131))
 model <- train(lrn, task)
 
 library(DALEXtra)
 
-explainer <- explain_mlr(model, data[,-c(1, 9)], data$price)
+explainer <- explain_mlr(model, phones[,-c(1, 9)], phones$price)
--- scripts/predict_parts_measure.R
+++ scripts/predict_parts_measure.R
@@ -1,15 +1,12 @@
 rd_question_data <- readRDS("result_question_3.rds")
 nd_question_data <- readRDS("result_question_2.rds")
 
-bd_measure <- function(bd, expl, expl_fi = NULL, id) {
-
+bd_measure <- function(bd, expl, expl_fi = NULL, id, M) {
  expl <- expl[expl$id == id,]
- expl <- round(sapply(expl, mean))[-8]
+ expl <- round(sapply(expl, mean))[-9]
+ bd <- bd[bd$variable != "intercept" & bd$variable != "prediction",]
 
-
- discretize <- quantile(-max(abs(bd$contribution)):max(abs(bd$contribution)),
-                        c(0.2, 0.4, 0.6, 0.8, 1))[-5]
- discretize <- c(-max(abs(bd$contribution))-1, discretize, max(abs(bd$contribution))+1)
+ discretize <- c(-Inf, -M/2, -M/10, M/10, M/2, Inf)
  bins <- cut(bd$contribution, breaks = discretize, labels = c(-2, -1, 0, 1, 2))
  bins <- as.numeric(as.character(bins))
  names(bins) <- bd$variable_name
@@ -21,7 +18,7 @@ bd_measure <- function(bd, expl, expl_fi = NULL, id) {
    return(norm_score)
  }
 
- expl_fi <- unlist(expl_fi[expl_fi$id == id,-8][1,])
+ expl_fi <- unlist(expl_fi[expl_fi$id == id,-9][1,])
  bd_fi <- seq_along(bd$variable_name)
  names(bd_fi) <- bd$variable_name
  fi_score <- norm(expl_fi/7 - bd_fi[names(expl_fi)]/7, type = "2")
@@ -30,16 +27,13 @@ bd_measure <- function(bd, expl, expl_fi = NULL, id) {
 
 }
 
-shap_measure <- function(bd, expl, expl_fi = NULL, id) {
-
+shap_measure <- function(bd, expl, expl_fi = NULL, id, M) {
 
     expl <- expl[expl$id == id,]
-    expl <- round(sapply(expl, mean))[-8]
+    expl <- round(sapply(expl, mean))[-9]
 
     bd <- bd[bd$B == 0,]
-    discretize <- quantile(-max(abs(bd$contribution)):max(abs(bd$contribution)),
-                           c(0.2, 0.4, 0.6, 0.8, 1))[-5]
-    discretize <- c(-max(abs(bd$contribution))-1, discretize, max(abs(bd$contribution))+1)
+    discretize <- c(-Inf, -M/2, -M/10, M/10, M/2, Inf)
     bins <- cut(bd$contribution, breaks = discretize, labels = c(-2, -1, 0, 1, 2))
     bins <- as.numeric(as.character(bins))
     names(bins) <- bd$variable_name
@@ -51,7 +45,7 @@ shap_measure <- function(bd, expl, expl_fi = NULL, id) {
         return(norm_score)
     }
 
-    expl_fi <- unlist(expl_fi[expl_fi$id == id,-8][1,])
+    expl_fi <- unlist(expl_fi[expl_fi$id == id,-9][1,])
     bd_fi <- seq_along(bd$variable_name[order(bd$contribution, decreasing = TRUE)])
     names(bd_fi) <- bd$variable_name[order(bd$contribution, decreasing = TRUE)]
     fi_score <- norm(expl_fi/7 - bd_fi[names(expl_fi)]/7, type = "2")
@@ -60,18 +54,39 @@ shap_measure <- function(bd, expl, expl_fi = NULL, id) {
 
 }
 
+lime_measure <- function(lime, expl, expl_fi = NULL, id, M) {
+    expl <- expl[expl$id == id,]
+    expl <- round(sapply(expl, mean))[-9]
+    
+    lime <- lime[lime$original_variable != "",]
+    zero_variables <- setdiff(names(expl), lime$original_variable)
+    discretize <- c(-Inf, -M/2, -M/10, M/10, M/2, Inf)
+    contribtions <- c(
+        structure(lime$estimated,
+                  names = lime$original_variable),
+        structure(rep(0, times = length(zero_variables)),
+                  names = zero_variables)
+    )
+    bins <- cut(contribtions, breaks = discretize, labels = c(-2, -1, 0, 1, 2))
+    bins <- as.numeric(as.character(bins))
+    names(bins) <- names(contribtions)
+    
+    expl <- as.numeric(expl[names(bins)])
+    norm(bins - expl, type = "2")
+}
+
 ids_compound <- intersect(unique(rd_question_data$id), unique(nd_question_data$id))
 ids <- unique(rd_question_data$id)
 
 ppsv_ibd <- sapply(ids_compound, function(x) {
-    bd <- predict_parts(explainer, data[x,], type = "break_down")[-c(1,9),]
-    bd_measure(bd, rd_question_data, nd_question_data, x)
+    bd <- predict_parts(explainer, phones[x,], type = "break_down")
+    bd_measure(bd, rd_question_data, nd_question_data, x, M = mean(phones$price))
 })
 
 
 pps_ibd <- sapply(ids, function(x) {
-    bd <- predict_parts(explainer, data[x,], type = "break_down")[-c(1,9),]
-    bd_measure(bd, rd_question_data, NULL, x)
+    bd <- predict_parts(explainer, phones[x,], type = "break_down")
+    bd_measure(bd, rd_question_data, NULL, x, M = mean(phones$price))
 })
 names(pps) <- ids
 
@@ -79,8 +94,8 @@ ppsv_shap <- lapply(c(10, 20, 30), function(z) {
     sapply(ids_compound, function(x) {
         ibds <- sapply(1:10, function(y) {
             print(paste(z, y))
-            bd <- predict_parts(explainer, data[x,], type = "shap", B = z)
-            shap_measure(bd, rd_question_data, nd_question_data, x)
+            bd <- predict_parts(explainer, phones[x,], type = "shap", B = z)
+            shap_measure(bd, rd_question_data, nd_question_data, x, M = mean(phones$price))
         })
         mean(ibds)
     })
@@ -90,11 +105,16 @@ pps_shap <- lapply(c(10, 20, 30), function(z) {
     sapply(ids, function(x) {
         ibds <- sapply(1:10, function(y) {
             print(paste(z, y))
-            bd <- predict_parts(explainer, data[x,], type = "shap", B = z)
-            shap_measure(bd, rd_question_data, NULL, x)
+            bd <- predict_parts(explainer, phones[x,], type = "shap", B = z)
+            shap_measure(bd, rd_question_data, NULL, x, M = mean(phones$price))
         })
         mean(ibds)
     })
+})
+
+pps_lime <- sapply(ids, function(x) {
+    bd <- DALEXtra::predict_surrogate_local_model(explainer, phones[x,])
+    lime_measure(bd, rd_question_data, NULL, x, M = mean(phones$price))
 })
 
 # data.frame(ID = ids, PPS = pps, PPSv = ppsv_imputed, Price = data$price[ids]) -> ibd_table
